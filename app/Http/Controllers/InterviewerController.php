1544f54a8683492fbd9ee461bafaf80f0258fadc
prasid444 <prasidkarki95@live.com>
2018-05-25 18:42:00 +0545

    Controller file added

---
+++ app/Http/Controllers/InterviewerController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+/**
+ *
+ * It includes all the functions that are to be performed in interviewer page
+ */
+class InterviewerController extends Controller
+{
+    //
+    //
+
+    function selectInterviewee(){
+
+    }
+    function submitScore(){
+
+    }
+    function getDocument()
+    {
+      # code...
+    }
+    function getAllScore(){
+
+    }
+    function viewIntervieweeDetails(){
+
+    }
+    function getMyAssignedInterview(){
+
+    }
+
+    function openInterview(){
+
+    }
+    function getOtherInterviewer(){
+
+    }
+
+    function viewMyProfile(){
+
+    }
+
+}
